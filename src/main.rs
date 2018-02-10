8e3077d87a6ff690e4ccbcdf8e846c4762f5c2de
xuxiaoyu2010 <xuxiaoyu2010@gmail.com>
2018-02-10 17:55:26 +0800

    complete

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use std::env;
+use std::path::Path;
+use std::fs;
+
+fn read_dir<P: AsRef<Path>>(p: P , prefix: &str) {
+    if let Ok(entries) = fs::read_dir(p) {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                print!("{}", prefix);
+                if let Ok(name) = entry.file_name().into_string() {
+                    println!("|____{}", name);
+                }
+
+                let path = entry.path();
+                if path.is_dir() {
+                    let next_prefix = "| ".to_string() + prefix;
+                    read_dir(&path, &next_prefix);
+                }
+            }
+        }
+    }
+}
+
+
+
+fn main() {
+    
+    let mut args: Vec<String> = env::args().collect();
+    let mut param :Vec<&str> = vec![];
+    let leng = args.len();
+    
+    if leng == 1  {
+        param.push(".");
+    } else {
+        args.remove(0);
+        for element in args.iter() {
+            param.push(element);
+        }
+    }
+
+    for p in &param {
+
+        let pa = Path::new(p);
+        println!("{}",pa.display());        
+        read_dir(&pa, "");
+    }
+
+}
