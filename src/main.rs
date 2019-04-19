0d1949c01b49ccfe595391fe03679766c005eaf9
xuxy <xuxiaoyu2010@gmail.com>
2019-04-19 17:50:05 +0800

    add option -a

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,92 @@
-use std::env;
-use std::path::Path;
+extern crate clap;
+use clap::{Arg, App};
+
+use std::path::{Path, PathBuf};
 use std::fs;
 
-fn read_dir<P: AsRef<Path>>(p: P , prefix: &str) {
+
+fn read_dir<P: AsRef<Path>>(p: P , is_all:bool, prefix: &str) {
     if let Ok(entries) = fs::read_dir(p) {
-        let hidden = entries.filter(| x | {
-            if let Ok(y) = x {
-                return !y.file_name().into_string().unwrap().starts_with(".");
-            }
-            return true;
-        });
-        
-        for entry in hidden {
-            if let Ok(entry) = entry {
-                print!("{}", prefix);
-                if let Ok(name) = entry.file_name().into_string() {
-                    println!("|____{}", name);
+
+        if !is_all {
+            let hidden = entries.filter(| x | {
+                if let Ok(y) = x {
+                    return !y.file_name().into_string().unwrap().starts_with(".");
                 }
-                
-                let path = entry.path();
-                if path.is_dir() {
-                    let next_prefix = "| ".to_string() + prefix;
-                    read_dir(&path, &next_prefix);
+                return true;
+            });
+            for entry in hidden {
+                if let Ok(entry) = entry {
+                    print!("{}", prefix);
+                    if let Ok(name) = entry.file_name().into_string() {
+                        println!("|____{}", name);
+                    }
+                    
+                    let path = entry.path();
+                    if path.is_dir() {
+                        let next_prefix = "| ".to_string() + prefix;
+                        read_dir(&path, is_all, &next_prefix);
+                    }
                 }
-            }
-        }
+            }            
+        } else {
+            for entry in entries {
+                if let Ok(entry) = entry {
+                    print!("{}", prefix);
+                    if let Ok(name) = entry.file_name().into_string() {
+                        println!("|____{}", name);
+                    }
+                    
+                    let path = entry.path();
+                    if path.is_dir() {
+                        let next_prefix = "| ".to_string() + prefix;
+                        read_dir(&path, is_all, &next_prefix);
+                    }
+                }
+            }        
+        }       
+
     }
 }
 
 fn main() {
     
-    let mut args: Vec<String> = env::args().collect();
-    let mut param :Vec<&str> = vec![];
-    let leng = args.len();
+    let opt = args();
+    println!("{}",opt.path.display());
+    read_dir(&opt.path, opt.all,"");
     
-    if leng == 1  {
-        param.push(".");
-    } else {
-        args.remove(0);
-        for element in args.iter() {
-            param.push(element);
-        }
-    }
-
-    for p in &param {
-
-        let pa = Path::new(p);
-        println!("{}",pa.display());        
-        read_dir(&pa, "");
-    }
-
 }
+
+struct Opt {
+    all: bool,
+    path: PathBuf,
+}
+
+fn args () -> Box<Opt> {
+    let matches = App::new("Tree")
+        .version("1.0")
+        .author("xuxy")
+        .about("Show the directory as tree")
+        .arg(Arg::with_name("INPUT")
+             .help("Sets the input file to use")
+             .default_value(".")
+             .index(1))
+        .arg(Arg::with_name("all")
+             .short("a")
+             .help(""))
+        .get_matches();
+
+    let mut pth: &str = ".";
+    let mut all: bool = false;
+    
+    if let Some(files) = matches.value_of("INPUT") {
+        pth = files
+    }
+    
+    if matches.is_present("all") {
+        all = true
+    }
+    
+    Box::new(Opt{all:all, path: PathBuf::from(pth)})
+}
+
